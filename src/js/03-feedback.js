7a94b855ee03cc135796d2d93144a983e50a01e3
Yevgeniia Pererva <jane.iljasowa@gmail.com>
2022-12-28 20:06:27 +0200

    add ex 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener(`input`, throttle(onFormInput, 500));
+form.addEventListener(`submit`, onFormSubmit);
+
+const formData = {};
+
+function onFormInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
+}
+
+
+function getTextStorage() {
+    let saveInput = localStorage.getItem(STORAGE_KEY);
+
+    if (!saveInput) {
+            return;
+    }
+    
+    Object.entries(saveData).forEach(
+        ([name, value]) => (form.elements[name].value = value)
+    );
+}; getTextStorage();
+
 
